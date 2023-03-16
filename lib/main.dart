c4d40c3e9c3799fc6c2c383f6c12711de16380e4
Faris Basha <fbashatm@gmail.com>
2023-03-16 21:50:37 +0530

    theme change

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
+import 'package:chat_gpt_clone/controllers/home_controller.dart';
 import 'package:chat_gpt_clone/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+  final c = Get.put(HomeController());
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
         title: 'Flutter Demo',
-        theme: ThemeData.dark(useMaterial3: true),
+        theme: ThemeData.light(),
         home: const HomePage());
   }
 }
